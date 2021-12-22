92a8033482b22bbe3d508d44a7d7fc83f4b24af6
John Hoffer <john@hoff.in>
2021-12-22 00:01:42 -0600

    improve tests

--- tests/index.js
+++ tests/index.js
@@ -2,51 +2,28 @@ import { suite } from "uvu";
 import * as assert from "uvu/assert";
 import * as ENV from "./setup/env.js";
 import { useHashHistory } from "../src/index.ts";
+import { createPath } from "history";
 
-const TestUseHistory = suite("test history hook");
+/*
+ * Test rendering with react-router-dom
+ */
+const testExample = (key, props) => {
+  const TestExample = suite(`test ${key} with react-router-dom`);
+  TestExample.before.each(ENV.reset);
 
-// Create and clean the DOM
-TestUseHistory.before.each(ENV.reset);
+  TestExample("Example renders", async () => {
+    const { innerHTML } = ENV.renderExample(props);
+    const expected = ENV.expectExample(props);
+    assert.snapshot(innerHTML, expected);
+  });
 
-TestUseHistory("Example renders", async () => {
-  const { innerHTML } = ENV.renderExample({});
-  const expected = ENV.expectExample({});
-  assert.snapshot(innerHTML, expected);
-});
-
-TestUseHistory("#a=1#b=2 renders", async () => {
-  const props = {
-    options: { hashRoot: "", hashSlash: "#" },
-    routes: ["home", "a=1/b=2"],
-  };
-  const { innerHTML } = ENV.renderExample(props);
-  const expected = ENV.expectExample(props);
-  assert.snapshot(innerHTML, expected);
-});
-
-TestUseHistory("#!/hello/world renders", async () => {
-  const props = {
-    options: { hashRoot: "!/", hashSlash: "/" },
-    routes: ["home", "hello", "hello/world"],
-  };
-  const { innerHTML } = ENV.renderExample(props);
-  const expected = ENV.expectExample(props);
-  assert.snapshot(innerHTML, expected);
-});
-
-const getTestData = ({ options, fn, pathList }) => {
-  const history = useHashHistory(options);
-  return {
-    fn: history[fn],
-    listen: history.listen,
-    input: "/" + pathList.join("/"),
-  };
+  return TestExample;
 };
 
 /*
- * Roundtrip test of pathname update
+ * Roundtrip pathname update
  */
-const testLocationPathname = async ({ fn, listen, input }) => {
+const updatePathname = async ({ fn, listen, input }) => {
   return new Promise((resolve) => {
     listen(({ location }) => {
       resolve(location.pathname);
@@ -55,64 +32,66 @@ const testLocationPathname = async ({ fn, listen, input }) => {
   });
 };
 
-TestUseHistory(`hashRoot="" push`, async () => {
-  const data = getTestData({
-    options: {
-      hashRoot: "",
-    },
-    fn: "push",
-    pathList: ["test"],
-  });
-  const result = await testLocationPathname(data);
-  assert.is(result, "/test");
-});
+const testUseHistory = (key, options) => {
+  const TestUseHistory = suite(`test ${key} history listener`);
+  let history;
 
-TestUseHistory(`hashRoot="" replace`, async () => {
-  const data = getTestData({
-    options: {
-      hashRoot: "",
-    },
-    fn: "replace",
-    pathList: ["test"],
+  // Create and clean the DOM
+  TestUseHistory.before.each(ENV.reset);
+  TestUseHistory.before.each(() => {
+    history = useHashHistory(options);
   });
-  const result = await testLocationPathname(data);
-  assert.is(result, "/test");
-});
 
-TestUseHistory(`hashRoot="/" push`, async () => {
-  const data = getTestData({
-    options: {
-      hashRoot: "/",
-    },
-    fn: "push",
-    pathList: ["test"],
+  // Test window location at every step
+  TestUseHistory.after.each(() => {
+    const { hashRoot = "" } = options;
+    const historyHref = createPath(history.location);
+    const windowHref = window.location.hash.substr(1);
+    expect(historyHref.replace(/^\//, hashRoot)).toEqual(windowHref);
   });
-  const result = await testLocationPathname(data);
-  assert.is(result, "/test");
-});
 
-TestUseHistory(`hashRoot="/" replace`, async () => {
-  const data = getTestData({
-    options: {
-      hashRoot: "/",
-    },
-    fn: "replace",
-    pathList: ["test"],
+  TestUseHistory(`push /test`, async () => {
+    const result = await updatePathname({
+      fn: history.push,
+      listen: history.listen,
+      input: "/test",
+    });
+    assert.is(result, "/test");
   });
-  const result = await testLocationPathname(data);
-  assert.is(result, "/test");
-});
 
-TestUseHistory(`hashRoot="/" push ../test`, async () => {
-  const data = getTestData({
-    options: {
-      hashRoot: "/",
-    },
-    fn: "push",
-    pathList: ["..", "test"],
+  TestUseHistory(`replace /test`, async () => {
+    const result = await updatePathname({
+      fn: history.replace,
+      listen: history.listen,
+      input: "/test",
+    });
+    assert.is(result, "/test");
   });
-  const result = await testLocationPathname(data);
-  assert.is(result, "../test");
-});
 
-TestUseHistory.run();
+  TestUseHistory(`push ../test`, async () => {
+    const result = await updatePathname({
+      fn: history.push,
+      listen: history.listen,
+      input: "../test",
+    });
+    assert.is(result, "../test");
+  });
+
+  return TestUseHistory;
+};
+
+// Run the tests
+[
+  testExample("default", {}),
+  testExample("#a=1#b=2", {
+    options: { hashRoot: "", hashSlash: "#" },
+    routes: ["home", "a=1/b=2"],
+  }),
+  testExample("!/hello/world", {
+    options: { hashRoot: "!/", hashSlash: "/" },
+    routes: ["home", "hello", "hello/world"],
+  }),
+  testUseHistory("default", {}),
+  testUseHistory("relative", { hashRoot: "" }),
+  testUseHistory("absolute", { hashRoot: "/" }),
+].map((test) => test.run());
